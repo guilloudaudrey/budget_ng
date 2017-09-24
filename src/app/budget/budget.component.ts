4abe5b361cc9a052f47fe16196c0eb9cb9a0feeb
Audrey Guilloud <audrey.guilloud2201@gmail.com>
2017-09-24 18:58:41 +0200

    soustraction

--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { Operation } from './classes/Operation';
-import { Depot } from './classes/Depot'
-import { Retrait } from './classes/Retrait'
 import { OperationService } from '../shared/operation.service';
 import { OperationAjaxService } from '../shared/operation-ajax.service';
 
@@ -12,48 +10,49 @@ import { OperationAjaxService } from '../shared/operation-ajax.service';
 })
 export class BudgetComponent {
 solde = 1000;
+sum = 0;
 opes;
+virements;
 virmontant:number;
 virdescription:string;
-retmontant:number;
+retmontant:number = this.retmontant* -1;
 retdescription:string;
 
+constructor(private operationService:OperationService, private operationAjaxService:OperationAjaxService){}
 
-
-
-// ajoutDep() {
-//   this.operationService.ajoutDepot(new Depot(new Date, this.virdescription, this.virmontant));
-// }
-
-// ajoutRetr(){
-//   this.operationService.ajoutRetrait(new Retrait(new Date, this.retdescription, this.retmontant));
-// }
-
-// supp(index:number){
-//   this.operationService.supprimer(index);
-// }
-
-
-
-getSolde(){
-  this.solde = this.operationService.getSolde();
-
+ngOnChange(){
+  
 }
 
-constructor(private operationService:OperationService, private operationAjaxService:OperationAjaxService){
-  this.opes = this.operationService.opes;
-
-  }
-
 ngOnInit() {
-  this.operationAjaxService.getAllOpes().then((todos) => this.opes = <Object[]>todos);
+  this.operationAjaxService.getAllOpes().then((operations) => this.opes = <Object[]>operations);
+
+   
   }
 
-  addOpe(){
-    this.operationAjaxService.addOpe({date: new Date, montant: this.virmontant, description: this.virdescription}).then((operation) => this.opes.push(operation));;
+  addOpe(montant, description){
+    this.operationAjaxService.addOpe({
+      date: new Date, 
+      montant: montant, 
+      description: description
+    })
+      .then((operation) => this.opes.push(operation));;
   }
 
   removeOpe(index:number){
-    this.operationAjaxService.removeOpe({id: index, date: new Date, montant: 0, description: 'null'}).then(() => this.opes = this.opes.filter((operation) => operation.id !=index) );
+    this.operationAjaxService.removeOpe({
+      id: index, 
+      date: new Date, 
+      montant: 0, 
+      description: 'null'
+    })
+    .then(() => this.opes = this.opes.filter((operation) => operation.id !=index) );
   }
-}
+
+  getSolde(){
+this.solde = this.operationService.getSolde(this.solde, this.opes, this.sum);
+    }
+  }
+  
+
+
--- src/app/shared/operation.service.ts
+++ src/app/shared/operation.service.ts
@@ -1,34 +1,23 @@
 import { Injectable } from "@angular/core";
 import {Operation} from '../budget/classes/Operation'
+import { OperationAjaxService } from "./operation-ajax.service";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 
 export class OperationService {
   solde = 1000;
+  sum = 0;
   opes = [];
+  
     
-   
-    getSolde():number {
-        let solde = this.solde;
-        for(let ope of this.opes){
-          solde += ope.montant;   
-        }
-     
-        return solde;
-    }
- 
-    ajoutDepot(operation:Operation) {
-      this.opes.push(operation);
-    }
-    
-    // ajoutRetrait(operation:Operation){
-    //   this.retraits.push(operation);
-    // }
-    
-    supprimer(index:number){
-      this.opes.splice(index,1);
-    
-    }
-    
+  constructor(private operationAjaxService:OperationAjaxService){}
 
+    getSolde(solde, opes, sum) {   
+       
+        for(let ope of opes){
+          sum += ope.montant; 
+        }      
+       return sum;
+    }
 }
